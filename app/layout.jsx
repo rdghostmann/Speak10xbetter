feb4e5cd5f4e17d48b1f8a79943bffd2c6e23996
rdghostmann <randalchukzwilson@gmail.com>
2025-07-26 04:41:19 +0100

    added favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,11 +16,17 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Speak10xBetter",
   description: "Master the Art of Public Speaking From stage fright to standing ovations. I help professionals, entrepreneurs, and leaders speak with confidence and impact.",
+  icons: {
+    icon: "/speaker-favicon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <Head>
+        <link rel="icon" href="/speaker-favicon.png" />
+      </Head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} cz-shortcut-listen="true">
         {children}
       </body>
