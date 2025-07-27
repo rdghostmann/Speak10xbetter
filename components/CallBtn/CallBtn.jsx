adb86b5b3e4c6becea542517f4acb29f3edd5995
rdghostmann <randalchukzwilson@gmail.com>
2025-07-27 14:22:41 +0100

    Added URL Blob VI and Call

---
+++ components/CallBtn/CallBtn.jsx
@@ -0,0 +1,16 @@
+import { PhoneCall } from 'lucide-react'
+import React from 'react'
+
+const CallBtn = () => {
+    return (
+        <a
+            href="tel:+234XXXXXXXXXX"
+            className="inline-flex items-center gap-2 bg-cyan-600 text-white px-6 py-4 rounded-xl text-lg hover:bg-cyan-500 transition"
+        >
+            <PhoneCall className="w-10 h-10" />
+            Call Now
+        </a>
+    )
+}
+
+export default CallBtn
