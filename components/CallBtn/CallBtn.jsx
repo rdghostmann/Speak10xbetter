23087964b29f016f8fc5992802bb5e64ba69c202
rdghostmann <randalchukzwilson@gmail.com>
2025-07-27 15:56:29 +0100

    Added icons FaTiktok

--- components/CallBtn/CallBtn.jsx
+++ components/CallBtn/CallBtn.jsx
@@ -7,7 +7,7 @@ const CallBtn = () => {
             href="tel:+2349051071524"
             className="inline-flex items-center gap-2 bg-cyan-600 text-white px-6 py-4 rounded-xl text-lg hover:bg-cyan-500 transition"
         >
-            <PhoneCall className="w-5 h-15" />
+            <PhoneCall className="w-5 h-5" />
             Call Now
         </a>
     )
