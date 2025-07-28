a84a93b3e9740fd8d6c47bd7b37d771584e5ab63
rdghostmann <randalchukzwilson@gmail.com>
2025-07-28 15:15:34 +0100

    Update Stuff on CallBtnAction

--- components/CallBtn/CallBtn.jsx
+++ components/CallBtn/CallBtn.jsx
@@ -9,7 +9,7 @@ const CallBtn = () => {
             className="inline-flex items-center gap-2 bg-cyan-600 text-white px-3 py-4 rounded-sm text-lg hover:bg-cyan-500 transition"
         >
             <PhoneCall className="w-5 h-5" />
-            Call Now
+            Book a Call
         </Link>
     )
 }
