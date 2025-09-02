5d0c87bcbb1d9148dd2b1e40065432a52981776b
rdghostmann <randalchukzwilson@gmail.com>
2025-09-02 12:57:12 +0100

    Added Newsletters form

--- app/page.jsx
+++ app/page.jsx
@@ -5,25 +5,30 @@ import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import Hero from "@/components/Hero/Hero";
 import Stats from "@/components/Stats/Stats";
+import SubscribeForm from "@/components/SubscribeForm/SubscribeForm";
 import SuccessStories from "@/components/SuccessStories/SuccessStories";
 import Testimonials from "@/components/Testimonials/Testimonials";
 import VideoSection from "@/components/VideoSection/VideoSection";
 
 export default function Home() {
   return (
-     <>
-    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-blue-900 to-slate-900">
-      <Header />
-      <Hero />
-      <About />
-      {/* <VideoSection /> */}
-      <Stats />
-      <SuccessStories />
-      <Testimonials />
-      <CallToAction />
-      <FAQ />
-      <Footer />
-    </div>
-     </>
+    <>
+      <div className="min-h-screen bg-gradient-to-br from-slate-900 via-blue-900 to-slate-900">
+        <Header />
+        <Hero />
+        <About />
+        {/* <VideoSection /> */}
+        <Stats />
+        <SuccessStories />
+        <Testimonials />
+        <div className="relative w-9/12 mx-auto py-4">
+          {/* Insert inline form before FAQ */}
+          <SubscribeForm />
+        </div>
+        <CallToAction />
+        <FAQ />
+        <Footer />
+      </div>
+    </>
   );
 }
